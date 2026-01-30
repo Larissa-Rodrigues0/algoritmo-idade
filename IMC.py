a2e45fb1863b1d4cf536602b21149f84a7b0d718
Larissa Rodrigues <larissarodrigues160107@gmail.com>
2026-01-29 23:45:40 -0300

    IMC

---
+++ IMC.py
@@ -0,0 +1,17 @@
+print(" == Sistema para calcular IMC == \nIndique sua altura e peso: ")
+altura = float(input("Altura(m): "))
+peso = float(input("Peso(kg): "))
+ 
+resul = peso/(altura*altura)
+ 
+if resul < 18.5:
+    print(f"O resultado do calculo foi {resul:.2f}\nPortanto sua classificação é abaixo do peso")
+elif resul < 24.9:
+    print(f"O resultado do calculo foi {resul:.2f}\nPortanto sua classificação é peso normal")
+elif resul < 29.9:
+    print(f"O resultado do calculo foi {resul:.2f}\nPortanto sua classificação é Sobrepeso")
+elif resul < 39.9:
+    print(f"O resultado do calculo foi {resul:.2f}\nPortanto sua classificação é Obesidade")
+else:
+    print(f"O resultado do calculo foi {resul:.2f}\nPortanto sua classificação é Obesidade Grave")
+ 
